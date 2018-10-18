87dc22d37ab049451c378cc9bf4a1f432a819965
tony-ch <changtao996@gmail.com>
2018-10-18 16:34:54 +0800

    add parfor and resize img to accelerate

--- run_subdir.m
+++ run_subdir.m
@@ -1,13 +1,12 @@
 function run_subdir(input_dir, save_dir, para)
-%RUN_SUBDIR 此处显示有关此函数的摘要
-%   此处显示详细说明
 if ~exist(save_dir,'dir')
     mkdir(save_dir)
 end
 fprintf('run in %s\n',input_dir);
 imgs = fullfile(input_dir,'*.jpg');
 imgs = dir(imgs);
-for i = 2 : length(imgs)
+% change to parfor to use multi cpu
+parfor i = 2 : length(imgs)
     im1_name = imgs(i-1).name;
     im2_name = imgs(i).name;
     flow_name = [im1_name(1:strfind(im1_name,'.')-1) '_' im2_name(1:strfind(im2_name,'.')-1) '_flow.jpg'];
@@ -19,12 +18,20 @@ for i = 2 : length(imgs)
     
     im1 = im2double(imread(fullfile(input_dir, im1_name)));
     im2 = im2double(imread(fullfile(input_dir, im2_name)));
+    
+    % resize to 300x300 to speed up
+    fprintf('resize to [300,300]\n')
+    im1 = imresize(im1,[300,300]);
+    im2 = imresize(im2,[300,300]);
     tic
     [vx,vy,warpI2] = Coarse2FineTwoFrames(im1,im2,para);
     toc
     %clear flow;
-    flow(:,:,1) = vx;
-    flow(:,:,2) = vy;
+    % remove to use parfor
+    %flow(:,:,1) = vx;
+    %flow(:,:,2) = vy;
+    
+    flow = cat(3,vx,vy);
     imflow = flowToColor(flow);
     
     %figure;imshow(im1);
